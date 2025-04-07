d5a6c1ae52852747a71f9293b28fcc79b6f14801
quartzs2 <csh001231@gmail.com>
2025-04-07 10:35:29 +0900

    정답 1

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,16 @@ export default function Card({ item }) {
     //2. item의 title, description, category를 각각 h2, p, span태그에 알맞게 넣으세요
     //3. item은 CardList에서 전달됩니다.
     //* 먼저 CardList.jsx를 작업해야 합니다!
-
+    const { title, description, category } = item;
     return (
         <div className="card">
             <div className="card-image">
                 <img src={`https://placehold.co/600x400`} />
             </div>
             <div className="card-content">
-                <span className="card-category">???</span>
-                <h2 className="card-title">???</h2>
-                <p className="card-description">???</p>
+                <span className="card-category">{category}</span>
+                <h2 className="card-title">{title}</h2>
+                <p className="card-description">{description}</p>
             </div>
         </div>
     );
